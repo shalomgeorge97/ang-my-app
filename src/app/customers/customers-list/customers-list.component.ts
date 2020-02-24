a93807d4a31e28fdb7a47a10afb6d063cb3bb333
Shalom <shalomgeorge97@gmail.com>
2020-02-24 18:41:04 +0530

    3rd commit

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { ICustomer } from "src/app/shared/interfaces";
 
 @Component({
@@ -10,8 +10,31 @@ export class CustomersListComponent implements OnInit {
   filteredCustomers: ICustomer[] = [];
   customersOrderTotal: number;
   currencyCode: string = "USD";
+  private _customers: ICustomer[] = [];
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
 
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // this.filteredCustomers = this.customers;
+  }
+
+  calculateOrders() {
+    this.customersOrderTotal = 0;
+    this.filteredCustomers.forEach((cust: ICustomer) => {
+      this.customersOrderTotal += cust.orderTotal;
+    });
+  }
+
+  sort(prop: string) {
+    // A sorter service will handle the sorting
+  }
 }
