18ef6b8489e444347b7b1005a8ef8bdc5dd83b32
Shalom <shalomgeorge97@gmail.com>
2020-02-25 18:35:44 +0530

    4th commit

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
+
 import { CustomersComponent } from "./customers.component";
 import { CustomersListComponent } from "./customers-list/customers-list.component";
 import { FilterTextboxComponent } from "./customers-list/filter-list.component";
+import { from } from "rxjs";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,7 @@ import { FilterTextboxComponent } from "./customers-list/filter-list.component";
     CustomersListComponent,
     FilterTextboxComponent
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, SharedModule],
   exports: [CustomersComponent, CustomersListComponent, FilterTextboxComponent]
 })
 export class CustomerModule {}
