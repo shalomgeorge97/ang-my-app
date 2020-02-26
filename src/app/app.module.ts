09f3e939732002b6fb4f1d2358f4f7af1ff5edf1
Shalom <shalomgeorge97@gmail.com>
2020-02-26 17:37:56 +0530

    Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { CoreModule } from "./core/core.module";
 
 import { AppComponent } from "./app.component";
 import { CustomerModule } from "./customers/customers.module";
@@ -7,7 +8,7 @@ import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CustomerModule, SharedModule],
+  imports: [BrowserModule, CustomerModule, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
